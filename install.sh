2b4b1c1c70bc49ac32ea5ddc375376cd84e2a7b8
jeevantk <jeevan.thomaskoshy@gmail.com>
2017-03-14 06:02:08 +0530

    Added ubuntu installtion after script

---
+++ install.sh
@@ -0,0 +1,10 @@
+echo "This script has to be run after ubuntut installation in order to install essential apps"
+sudo apt-get update
+sudo apt-get upgrade
+sudp apt-get install vlc
+sudo apt-get install git
+sudo add-apt-repository ppa:webupd8team/sublime-text-3
+sudo apt-get update
+sudo apt-get install sublime-text-installer
+
+
