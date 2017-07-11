8b42d18bd8c001f26fea48f34e68615a5ff5258e
jeevantk <jeevan.thomaskoshy@gmail.com>
2017-07-11 15:29:15 +0530

    Work

--- install.sh
+++ install.sh
@@ -14,6 +14,22 @@ sudo -H pip install jupyter
 sudo pip install sklearn
 sudo pip install xgboost
 sudo pip install tensorflow
+sudo apt-get install python-matplotlib
+sudo pip install BeautifulSoup
+sudo pip install seaborn
+sudo apt-get install python-urllib3
+sudo apt-get update && sudo apt-get install octave
+sudo pip install lightgbm
+sudo apt-get install libopencv-dev python-opencv
+sudo pip install tqdm
+sudo pip install futures
+sudo pip install keras
+#Installing Google Chrome
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt-get update 
+sudo apt-get install google-chrome-stable
+
 
 
 
@@ -57,3 +73,4 @@ sudo pip install tensorflow
 
 
 
+#Jayabhhs
